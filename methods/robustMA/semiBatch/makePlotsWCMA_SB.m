ffb769be939c3df7c121a044e0e3e363f84a2ad4
Jack <hillout177@gmail.com>
2022-10-03 15:09:09 +0100

    Create makePlotsWCMA_SB.m

---
+++ methods/robustMA/semiBatch/makePlotsWCMA_SB.m
@@ -0,0 +1,12 @@
+% makePlotsWCMA_SB makes the plots for:     "WCMA"
+% on the case study:                        "Semi-batch Bioreactor" 
+% used in:                                  "Chapter 6"
+%
+% 1. plant/model/obj/con functions are set-up,
+% 2. figures are set-up,
+% 3. axes are set-up,
+% 4. MA scheme (K=0.5) is run and plotted,
+% 5. WCMA scheme (K=0.9) is run and plotted,
+% 6. plots are saved.
+
+~~~~~~ To DO ~~~~~~
